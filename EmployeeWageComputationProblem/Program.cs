e6c7b0eec2dde7b205f0d74b4a291a782236ca53
kiran-sasaki <kiransekar1111@gmail.com>
2022-12-28 15:39:19 +0530

    [Kiran] Add: Class Object Function

--- EmployeeWageComputationProblem/Program.cs
+++ EmployeeWageComputationProblem/Program.cs
@@ -4,7 +4,8 @@
     {
         public static void Main(string[] args)
         {
-            EmployeeWage.EmpWage();
+            EmployeeWage employee = new EmployeeWage();
+            employee.EmpWage();
         }
     }
 }
