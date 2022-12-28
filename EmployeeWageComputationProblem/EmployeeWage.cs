dbb2f46d3b7eb090aa5193dacb17aeb8524d0ced
kiran-sasaki <kiransekar1111@gmail.com>
2022-12-28 12:53:07 +0530

    [Kiran] Add: Random Class

---
+++ EmployeeWageComputationProblem/EmployeeWage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputationProblem
+{
+    public static class EmployeeWage
+    {
+        public static void EmpWage()
+        {
+            int present = 1;
+            Random random = new Random();
+            int empCheck=random.Next(0, 2);
+            if(empCheck == present)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
