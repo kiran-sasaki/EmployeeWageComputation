0e1ba932d583d7249f4de2025a9a4485aef7e336
kiran-sasaki <kiransekar1111@gmail.com>
2022-12-28 12:47:26 +0530

    [Kiran] Add: Welcome Message

---
+++ EmployeeWageComputationProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWageComputationProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Wage Problem");
+        }
+    }
+}
